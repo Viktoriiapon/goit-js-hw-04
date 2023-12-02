960f81bf833be558da651c28d530d577c5c51ac6
Viktoriiapon <viktoriyaponom@gmail.com>
2023-12-02 12:11:18 +0100

    tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,23 @@
-function slugify(title){
-   const titleWords = title.toLowerCase().split(` `); 
-   const slug = titleWords.join(`-`);
-   return slug;
+function isEnoughCapacity(products, containerSize){
+  let quantityProducts = 0;
+  for(const product of Object.values(products)){
+  quantityProducts += product; 
+   }
+   return quantityProducts <= containerSize;
+
 }
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); 
